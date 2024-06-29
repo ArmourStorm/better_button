eacce98f7901912d578fa6f44d1a5313661b239a
armourstorm <armourstorm@mailfence.com>
2024-06-30 01:58:12 +1200

    Example app complete.

--- testing_area/src/main.rs
+++ testing_area/src/main.rs
@@ -16,11 +16,35 @@ fn main() {
                 spawn_button
             ),
         )
+        // .add_systems(
+        //     Update,
+        //     respond_to_button_state.after(BButtonUpdateSet)
+        // )
         .add_systems(
-            Update,
-            respond_to_button_state.after(BButtonUpdateSet)
+            FixedUpdate,
+            respond_to_button_events,
         )
         .run();
+}
+
+fn respond_to_button_events(
+    mut query: Query<&mut BackgroundColor, With<BHoverState>>,
+    mut event_reader: EventReader<BHoverEvent>,
+) {
+    for event in event_reader.read() {
+        match event {
+            BHoverEvent::JustEntered { entity } => {
+                if let Ok(mut background_color) = query.get_mut(*entity) {
+                    background_color.0 = Color::YELLOW_GREEN;
+                }
+            }
+            BHoverEvent::JustExited { entity } => {
+                if let Ok(mut background_color) = query.get_mut(*entity) {
+                    background_color.0 = Color::WHITE;
+                }
+            }
+        }
+    }
 }
 
 fn respond_to_button_state(
@@ -28,7 +52,7 @@ fn respond_to_button_state(
 ) {
     for (state, mut background_color) in &mut query {
         if state.just_entered {
-            background_color.0 = Color::GREEN;
+            background_color.0 = Color::YELLOW_GREEN;
         }
         if state.just_exited {
             background_color.0 = Color::WHITE;
@@ -61,77 +85,3 @@ fn spawn_camera(mut commands: Commands) {
         }
     );
 }
-
-//
-// fn main() {
-//     App::new()
-//         .add_plugins(
-//             (
-//                 DefaultPlugins,
-//                 BButtonPlugin,
-//             )
-//         )
-//         .add_systems(
-//             Startup,
-//             (
-//                 spawn_camera,
-//                 spawn_b_button,
-//             ),
-//         )
-//         .add_systems(
-//             Update,
-//             respond_to_button_events,
-//         )
-//         .run();
-// }
-
-// fn spawn_b_button(mut commands: Commands) {
-//     commands.spawn(
-//         BButtonBundle {
-//             button_bundle: ButtonBundle {
-//                 style: Style {
-//                     width: Val::Px(125.0),
-//                     height: Val::Px(125.0),
-//                     align_self: AlignSelf::Center,
-//                     justify_self: JustifySelf::Center,
-//                     ..default()
-//                 },
-//                 ..default()
-//             },
-//             ..default()
-//         }
-//     );
-// }
-
-// fn respond_to_button_state(
-//     mut query: Query<(&BMouseOverState, &mut BackgroundColor)>
-// ) {
-//     for (under_state, mut background_color) in &mut query {
-//         if under_state.just_entered {
-//             background_color.0 = Color::GREEN;
-//         }
-//         if under_state.just_exited {
-//             background_color.0 = Color::WHITE;
-//         }
-//     }
-// }
-
-// fn respond_to_button_events(
-//     mut query: Query<&mut BackgroundColor, With<BPressState>>,
-//     mut event_reader: EventReader<BPressEvent>,
-// ) {
-//     for event in event_reader.read() {
-//         match event {
-//             BPressEvent::JustEntered { entity } => {
-//                 if let Ok(mut background_color) = query.get_mut(*entity) {
-//                     background_color.0 = Color::CYAN;
-//                 }
-//             }
-//             BPressEvent::JustExited { entity } => {
-//                 if let Ok(mut background_color) = query.get_mut(*entity) {
-//                     background_color.0 = Color::WHITE;
-//                 }
-//             }
-//         }
-//     }
-// }
